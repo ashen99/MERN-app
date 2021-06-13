5ae88e77ee0d62235264004234cbc380cabec68e
ashane perera <ashenperera51@yahoo.com>
2021-06-14 03:28:09 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+//set up the server
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Hi server started on port: ${PORT}`));
 
-app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
+//FwgTrAex1nIanNfU
+//mongodb+srv://Ash:<password>@cluster0.i5zcj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+//connect to mongoDB
+
+mongoose.connect(
+  process.env.MDB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) return console.error(err);
+    console.log("connected to MOngoDB");
+  }
+);
